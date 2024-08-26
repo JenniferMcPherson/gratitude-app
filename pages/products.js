1c7f928e9de95e15508c77732f8f552b96248419
JenniferMcPherson <jennifermcp@gmail.com>
2024-08-25 22:57:17 -0500

    Create products.js

---
+++ pages/products.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Products() {
+  return (
+    <div>
+      <h1>Our Products</h1>
+      <p>Explore our collection of personalized gifts from around the world.</p>
+      {/* We'll add a product list component here later */}
+    </div>
+  );
+}
