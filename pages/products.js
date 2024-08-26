a914d6693bd9df5440b75be1f68cb34419f4924b
JenniferMcPherson <jennifermcp@gmail.com>
2024-08-25 23:03:11 -0500

    Update products.js

--- pages/products.js
+++ pages/products.js
@@ -1,11 +1,22 @@
 import React from 'react';
+import Layout from '../components/Layout';
+import ProductCard from '../components/ProductCard';
+
+const sampleProducts = [
+  { name: "Handmade Scarf", description: "Beautiful scarf made by artisans in Peru", price: 49.99, origin: "Peru" },
+  { name: "Carved Wooden Box", description: "Intricate wooden box crafted in India", price: 79.99, origin: "India" },
+];
 
 export default function Products() {
   return (
-    <div>
+    <Layout>
       <h1>Our Products</h1>
       <p>Explore our collection of personalized gifts from around the world.</p>
-      {/* We'll add a product list component here later */}
-    </div>
+      <div className="product-list">
+        {sampleProducts.map((product, index) => (
+          <ProductCard key={index} {...product} />
+        ))}
+      </div>
+    </Layout>
   );
 }
