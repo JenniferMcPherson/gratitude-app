6e85497e78b1ed39421c5c23b17b2baf3ef12e53
JenniferMcPherson <jennifermcp@gmail.com>
2024-08-25 22:58:11 -0500

    Create about.js

---
+++ pages/about.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function About() {
+  return (
+    <div>
+      <h1>About Gratitude</h1>
+      <p>Gratitude is an app dedicated to connecting artisans from around the world with people who want to express their appreciation through meaningful, personalized gifts.</p>
+    </div>
+  );
+}
